15c868bf51c441370aaeed2c82d258e0a0971a12
mbrowngo <mbrown@mossbeachdevelopment.com>
2014-02-06 08:51:33 -0800

    Update preneodb.php

--- Connections/preneodb.php
+++ Connections/preneodb.php
@@ -2,10 +2,10 @@
 # FileName="Connection_php_mysql.htm"
 # Type="MYSQL"
 # HTTP="true"
-$hostname_preneodb = "kent.preneo.org";
-$database_preneodb = "preneodb";
-$username_preneodb = "dataconnect";
-$password_preneodb = "zaq!xsw2";
+$hostname_preneodb = "xxx";
+$database_preneodb = "xxx";
+$username_preneodb = "xxx";
+$password_preneodb = "xxx";
 $preneodb = mysql_pconnect($hostname_preneodb, $username_preneodb, $password_preneodb) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
 
